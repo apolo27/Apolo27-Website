7bdbbc93ebbf09360ad9e704226a0570b96cfea0
BugNetwork <rainelro21@gmail.com>
2024-10-09 21:03:07 -0400

    Mejora de la estructura del proyecto con la creacion de folder "pages" y el enlace de las rutas

---
+++ src/pages/stem-with-us.tsx
@@ -0,0 +1,10 @@
+import { Form } from "@/components/form";
+
+export default function StemWithUs() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <p>Stem With Us</p>
+      <Form />
+    </div>
+  );
+}
