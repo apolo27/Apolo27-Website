3ef24fbb0585098a60d653fc88954f86c003b9e8
Sebastian De Leon <sebastianipl0131@gmail.com>
2024-10-24 16:50:56 -0400

    moviendo

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import Link from 'next/link';
-import Navbar from './components/Nav/Navbar';
-import Footer from './components/Footer/Footer';
-import Background from './components/Background/Background';
-import { Chatbot } from './components/Chatbot';
+import Link from "next/link";
+import Navbar from "./components/Nav/Navbar";
+import Footer from "./components/Footer/Footer";
+import Background from "./components/Background/Background";
+import { Chatbot } from "./components/Chatbot";
 export default function Home() {
   return (
     <div className="">
-      <Navbar />
-      <Background />
-      <Chatbot isSidebarOpen={false} />
-      <Footer />
+      <p>landing</p>
     </div>
   );
 }
