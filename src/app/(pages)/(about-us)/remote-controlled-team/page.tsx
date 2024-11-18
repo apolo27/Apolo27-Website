f8da6535c99f9d353085e2c7e1029f218e8e37e8
Sebastian De Leon <sebastianipl0131@gmail.com>
2024-11-18 15:41:18 -0400

    wip

--- src/app/(pages)/(about-us)/remote-controlled-team/page.tsx
+++ src/app/(pages)/(about-us)/remote-controlled-team/page.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image"
 export default function RemoteControlledTeam() {
     return (
-        <p>Remote Controlled Team</p>
+        <div className="h-screen w-full ">
+            <Image 
+                src={"/images/about-us/RCROVER.png"}
+                alt="Remote Controlled Rover"
+                width={1920}
+                height={1080}
+            />
+        </div>
     )
 }
--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,51 +1,112 @@
 import React from "react";
 import Link from "next/link";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTiktok,
+  faXTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   return (
-    <footer className="footer z-10">
-    <div className="container">
-      <div className="row">
-        <div className="footer-col">
-          <h4>Apolo 27</h4>
-          <ul className="">
-                  <li><Link href="/#timeline">Our Shrefry</Link></li>
-                  <li><Link href="/#Members">Members</Link></li>
-                  <li><Link href="Telemetry">Telemetry</Link></li>
-                  <li><Link href="STEM">STEM</Link></li>
-                  <li><Link href="Manufacturing">Manufacturing</Link></li>
-                  <li><Link href="Simulation">HERC Simulation</Link></li>
-          </ul>
-        </div>
-        <div className="footer-col">
-          <h4>Data Dashboard</h4>
-          <ul>
-                   <li><Link href="DataDashboard#overview">Overview</Link></li>
-                   <li><Link href="DataDashboard#rover">Rover</Link></li>
-                   <li><Link href="DataDashboard#crewmembers">Crewmembers</Link></li>
-                   {/* <li><Link href="DataDashboard#environment">Environment</Link></li> */}
-                   <li><Link href="DataDashboard#sensors">Sensors</Link></li>
-          </ul>
-        </div>
-        <div className="footer-col">
-          <h4>Stem With Us</h4>
-          <ul>
-            <li><Link href="StemWithUs#blogs">Blog</Link></li>
-            <li><Link href="StemWithUs#events">Events Calendar</Link></li>
-            <li><Link href="ContactUs">Book a School Visit</Link></li>
-          </ul>
-        </div>
-        <div className="footer-col">
-          <h4>follow us</h4>
-          <div className="social-links">
-                   <a href="https://www.instagram.com/apolo27_rd/" target="_blank"><i className="fab fa-instagram"></i></a>
-                   <a href="https://www.youtube.com/@apolo2730" target="_blank"><i className="fab fa-youtube"></i></a>
-                   <a href="https://www.linkedin.com/company/apolo27/" target="_blank"><i className="fab fa-linkedin-in"></i></a>
-                   <a href="https://www.tiktok.com/@apolo27rd" target="_blank"><i className="fab fa-tiktok"></i></a>
+    <footer
+      className="flex z-50 p-10 bg-[#0d0f18]"
+      style={{ position: "relative", zIndex: 10 }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="footer-col">
+            <h4>Apolo 27</h4>
+            <ul className="">
+              <li>
+                <Link href="">Our Shrefry</Link>
+              </li>
+              <li>
+                <Link href="">Members</Link>
+              </li>
+              <li>
+                <Link href="">Telemetry</Link>
+              </li>
+              <li>
+                <Link href="">STEM</Link>
+              </li>
+              <li>
+                <Link href="">Manufacturing</Link>
+              </li>
+              <li>
+                <Link href="">HERC Simulation</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Data Dashboard</h4>
+            <ul>
+              <li>
+                <Link href="">Overview</Link>
+              </li>
+              <li>
+                <Link href="">Rover</Link>
+              </li>
+              <li>
+                <Link href="">Crewmembers</Link>
+              </li>
+              {/* <li><Link href="#environment">Environment</Link></li> */}
+              <li>
+                <Link href="">Sensors</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Stem With Us</h4>
+            <ul>
+              <li>
+                <Link href="">Blog</Link>
+              </li>
+              <li>
+                <Link href="">Events Calendar</Link>
+              </li>
+              <li>
+                <Link href="">Book a School Visit</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>follow us</h4>
+            <div className="social-links flex flex-wrap gap-5">
+              <FontAwesomeIcon
+                className="hover:cursor-pointer hover:scale-110 text-white"
+                icon={faTiktok}
+                size="2xl"
+              />
+              <FontAwesomeIcon
+                href="://www.instagram.com/apolo27_rd/"
+                className="hover:cursor-pointer hover:scale-110 text-pink-500"
+                icon={faInstagram}
+                size="2xl"
+              />
+              <FontAwesomeIcon
+                className="hover:cursor-pointer hover:scale-110 text-blue-500"
+                icon={faFacebook}
+                size="2xl"
+              />
+              <FontAwesomeIcon
+                href="://www.tiktok.com/@apolo27rd"
+                className="hover:cursor-pointer hover:scale-110 text-white"
+                icon={faXTwitter}
+                size="2xl"
+              />
+              <FontAwesomeIcon
+                href="://www.youtube.com/@apolo2730"
+                className="hover:cursor-pointer hover:scale-110 text-red-500"
+                icon={faYoutube}
+                size="2xl"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
- </footer>
+    </footer>
   );
 }
