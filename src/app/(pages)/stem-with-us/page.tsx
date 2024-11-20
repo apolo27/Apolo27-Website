27ab063771514548b04329ccffad6c19787e05ba
Sebastian Marrera <smarrera@reispsolutions.com>
2024-11-20 14:38:26 -0400

    init, removing typescript

---
+++ src/app/(pages)/stem-with-us/page.tsx
@@ -0,0 +1,10 @@
+import Background from "../../components/Background/Background";
+import { Form } from "../../components/form";
+
+export default function StemWithUs() {
+  return (
+    <>
+      <Background />
+    </>
+  );
+}
