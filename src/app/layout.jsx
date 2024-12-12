bdb7c5372611aa371f6178cf6e9f0053feda0cb2
bugNetwork <raynerro2107@gmail.com>
2024-12-12 13:25:33 -0400

    Mejoras e implementacion de NextUI

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import { Chatbot } from './components/Chatbot';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Starfield from './components/Starfield';
+import Providers from './providers/NextUI';
 
 export const metadata = {
   title: 'Apolo 27',
@@ -10,17 +11,19 @@ export const metadata = {
     'Primer equipo universitario de la Republica Dominicana en participar en la NASA Human Exploration Rover Challenge',
 };
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header />
-        <main className="flex flex-col min-h-screen overflow-hidden">
-          <Starfield />
-          {children}
-          <Chatbot isSidebarOpen={false} />
-        </main>
-        <Footer />
+        <Providers>
+          <Header />
+          <main className="flex flex-col min-h-screen overflow-hidden">
+            <Starfield />
+            {children}
+            <Chatbot isSidebarOpen={false} />
+          </main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
