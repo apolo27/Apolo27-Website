a0420e16ce3efa2a75b732a9f5fa8bff88316c25
Sebastian De Leon <sebastianipl0131@gmail.com>
2024-12-19 18:58:46 -0400

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,23 @@ const nextConfig = {
         REACT_APP_API_KEY: 'AIzaSyCzfVpVDOvqLXVKLMDPczp5BUcs2jzU7Dg',
         REACT_APP_CALENDAR_ID: 'apolo27rd@gmail.com',
         GEMINI_API_KEY: "AIzaSyAHtzOwh7VL80Kzp95zdyekrCTF4ypnv2U"
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'contrareloj.com.do',               
+            },
+            {
+                protocol: 'https',
+                hostname: 'uploads-ssl.webflow.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'concepto.de',
+                
+            }
+        ]
     }
 };
 
--- src/app/(pages)/stem-with-us/page.jsx
+++ src/app/(pages)/stem-with-us/page.jsx
@@ -1,35 +1,68 @@
+"use client";
+import "./StemWithUs.css";
+import "./BigCalendar.css";
+import "./MobileStemWithUs.css";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Form } from "../../components/form";
 import Slider from "../../components/Slider/Slider";
 import StemPlanet from "../../components/Stem-Planet/StemPlanet";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 
-export default async function StemWithUs() {
-  // CALENDAR
-  const GOOGLE_CALENDAR_URL = `https://www.googleapis.com/calendar/v3/calendars/${process.env.REACT_APP_CALENDAR_ID}/events?key=${process.env.REACT_APP_API_KEY}`
-  const eventsResponse = await fetch(GOOGLE_CALENDAR_URL).then((res) => res.json())
-  const events = eventsResponse.items
+import moment from "moment";
+import "moment/locale/en-gb";
+import "moment/locale/es";
 
+const localizer = momentLocalizer(moment);
 
-  // BLOGGER
-  const BLOGGER_URL = "https://www.googleapis.com/blogger/v3/blogs/4816302503592460113/posts?key=AIzaSyARD4hXPEU2klOLmJPjPDHP_tE6x7fzNOA"
-  const bloggerResponse = await fetch(BLOGGER_URL).then((res) => res.json())
-  const posts = bloggerResponse.items
+import { getEvents } from "./services/FetchCalendarEvents";
+import { getTutorials } from "./services/FetchYTVideos";
+import { getRecentVideos } from "./services/FetchYTVideos";
+import { getLastVideo } from "./services/FetchYTVideos";
+import { getBlogs } from "./services/FetchBlogs";
+import Image from "next/image";
+import Link from "next/link";
 
+export default function StemWithUs() {
+  const [events, setEvents] = useState([]);
+  const [eventToShow, setEventToShow] = useState(undefined);
+  const [tutorials, setTutorials] = useState([]);
+  const [recentVideos, setRecentVideos] = useState([]);
+  const [blogs, setBlogs] = useState([]);
 
-  // YOUTUBE
+  const handleSelectedEvent = (event) => {
+    setEventToShow(event)
+ }
 
-  // const keywords = ['tutorial', 'diy', 'DIY'];
-  // const keywordQuery = keywords.join('|');
-  // const tutorialsResponse = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyCuIUoZCBUGVAfDdqUMyENwfCJ9AytDMDg&channelId=UCUb8Jn33w9TgVRim_wwWoGA&q=${keywordQuery}&part=snippet,id&order=date&maxResults=4&type=video`).then((res) => res.json());
-  // const tutorials =  tutorialsResponse.items
-  // console.log("tutoriales: ", tutorials)
+  useEffect(() => {
+    if (sessionStorage.getItem("events") === null) {
+      getEvents(setEvents);
+      // console.log("api call events");
+    } else {
+      setEvents(JSON.parse(sessionStorage.getItem("events")));
+    }
+    
+    if (sessionStorage.getItem("tutorials") === null) {
+      getTutorials(setTutorials);
+      // console.log("api call tutorials");
+    } else {
+      setTutorials(JSON.parse(sessionStorage.getItem("tutorials")));
+    }
 
-  const recentVidsResponse = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyCuIUoZCBUGVAfDdqUMyENwfCJ9AytDMDg&channelId=UCUb8Jn33w9TgVRim_wwWoGA&q&part=snippet,id&order=date&maxResults=3&type=video`).then((res) => res.json());
-  const recentVids = recentVidsResponse.items
+    if (sessionStorage.getItem("recentVideos") === null) {
+      getRecentVideos(setRecentVideos);
+      // console.log("api call recent videos");
+    } else {
+      setRecentVideos(JSON.parse(sessionStorage.getItem("recentVideos")));
+    }
 
-  const lastVidResponse = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyCuIUoZCBUGVAfDdqUMyENwfCJ9AytDMDg&channelId=UCUb8Jn33w9TgVRim_wwWoGA&q&part=snippet,id&order=date&maxResults=1&type=video`).then((res) => res.json());
-  const lastVid = lastVidResponse.items
+    if (sessionStorage.getItem("blogs") === null) {
+      getBlogs(setBlogs);
+      // console.log("api call blogs");
+    } else {
+      setBlogs(JSON.parse(sessionStorage.getItem("blogs")));
+    }
+  }, []);
 
   return (
     <div className="relative flex flex-col items-center justify-between min-h-screen pt-16 pb-16">
@@ -38,15 +71,91 @@ export default async function StemWithUs() {
         style={{ backgroundImage: "url(/images/BackgroundGalaxy.webp)" }}
       ></div>
 
-
       <StemPlanet />
 
+      <section className="mb-10 z-10 w-full h-fit flex justify-center flex-wrap gap-5 text-white bg-cyan-950 bg-opacity-85 py-10 mr-28 rounded-r-[100px] overflow-clip">
+        {blogs.slice(0, 3).map((blog, i) => {
+          return (
+            <div
+              key={i}
+              className="z-10 w-80 bg-black bg-opacity-70 p-4 pb-6 rounded-2xl active:animate-pulse"
+            >
+              <Image
+                src={blog.imgURL}
+                alt={blog.title}
+                width="0"
+                height="0"
+                sizes="100vw"
+                style={{objectFit: "cover"}}
+                className="w-full h-48 z-10 rounded-2xl"
+              />
+              <div className="space-y-3 mb-2">
+                <p className="font-semibold">
+                  {new Date(blog.date).toLocaleString("es", {
+                    year: "numeric",
+                    day: "numeric",
+                    month: "long",
+                    hour: "numeric",
+                    minute: "numeric",
+                  })}
+                </p>
+                <p className="font-semibold text-2xl">{blog.title}</p>
+                <p className=" h-52 overflow-hidden  text-ellipsis">{blog.content[2].slice(0, 225)}...</p>
+              </div>
 
+              <Link
+                className="text-black bg-white px-3 py-2 rounded-xl font-semibold hover:bg-slate-100 "
+                href={blog.blogURL}
+              >
+                Leer más
+              </Link>
+            </div>
+          );
+        })}
+      </section>
 
-
+      <div className="calendar_container text-white items-center gap-4 w-full bg-cyan-950 bg-opacity-85 pb-16 ml-28 rounded-l-[100px] overflow-clip">
+        <Calendar
+          className="calendario w-5/6"
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          toolbar={true}
+          views={["month", "agenda"]}
+          onSelectSlot={(e) => handleSelectedEvent(e)}
+          onSelectEvent={(e) => handleSelectedEvent(e)}
+        />
+          {eventToShow !== undefined && (
+        <div className="bg-black bg-opacity-70 p-4 rounded-2xl w-1/6">
+            <div className="event">
+              <h2>{eventToShow.title}</h2>
+              <p>{eventToShow.location}</p>
+              <p>
+                {new Date(eventToShow.start).toLocaleString("es", {
+                  year: "numeric",
+                  day: "numeric",
+                  month: "long",
+                  hour: "numeric",
+                  minute: "numeric",
+                })}
+              </p>
+              <p>
+                {new Date(eventToShow.end).toLocaleString("es", {
+                  year: "numeric",
+                  day: "numeric",
+                  month: "long",
+                  hour: "numeric",
+                  minute: "numeric",
+                })}
+              </p>
+            </div>
+        </div>
+          )}
+      </div>
 
       {/* Sección de eventos */}
-      <Slider slides={recentVids}/>
+      <Slider />
 
       {/* Botón para mostrar el formulario en pantallas pequeñas */}
       <div id="form" className="mt-8 mb-8 relative z-10 flex justify-center">
